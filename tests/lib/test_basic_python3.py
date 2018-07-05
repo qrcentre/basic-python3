8229f92a9ca1eaa6b4b81f75a3a6614ef17d8ac2
ning <ningyuan.sg@gmail.com>
2018-07-05 14:53:51 +0000

    Add end script for part 1
    commit e950366aeb005fcad81a97f50e12a095739073da
    Author: ning <ningyuan.sg@gmail.com>
    Date:   Thu Jul 5 14:46:53 2018 +0000
    
        Use telegram_send in part 1's script
    
    commit 97ad8883cfece95d56a5e5b21e9928f78dbfe971
    Author: ning <ningyuan.sg@gmail.com>
    Date:   Thu Jul 5 12:03:37 2018 +0000
    
        Move config.py -> tests/mocks.py
    
    commit 708d03873a17b075727b5610cf457e104d7dd81d
    Author: ning <ningyuan.sg@gmail.com>
    Date:   Mon Jul 2 17:29:16 2018 +0000
    
        Add example script average_temperature
    
    commit 46d4a4519068487941a49a37e7437739aa4ad7c8
    Author: ning <ningyuan.sg@gmail.com>
    Date:   Mon Jul 2 16:45:14 2018 +0000
    
        Add weather_get_rand as api failure contingency

--- tests/lib/test_basic_python3.py
+++ tests/lib/test_basic_python3.py
@@ -4,35 +4,38 @@ import sys, os
 sys.path.append(os.getcwd())
 
 import re, time, vcr
-import config, lib.basic_python3
+import tests.mocks, lib.basic_python3
 
 def sanitise_telegram_url(request):
     request.uri = re.sub('(?<=telegram\.org/bot)[^/]*', '', request.uri)
-    request.uri = re.sub('(?<=\?chat_id=)\d+', '', request.uri)
     return request
 
 vcr = vcr.VCR(
     cassette_library_dir='tests/cassettes',
+    match_on=['host'],
+    filter_query_parameters=['chat_id'],
     before_record_request=sanitise_telegram_url
 )
 
-@vcr.use_cassette(match_on=['host'])
+@vcr.use_cassette()
 def test_telegram_get_me():
-    result_list = lib.basic_python3.telegram_whoami(config.TELEGRAM_KEY)
+    result_list = lib.basic_python3.telegram_whoami(tests.mocks.TELEGRAM_KEY)
     are_strings = all([ isinstance(x, str) for x in result_list ])
     are_non_empty = all([ len(x) > 0 for x in result_list ])
     assert isinstance(result_list, list)
     assert are_strings
     assert are_non_empty
 
-@vcr.use_cassette('tests/cassettes/test_telegram_send', match_on=['host'])
+@vcr.use_cassette()
 def test_telegram_send():
     result = lib.basic_python3.telegram_send(
-        config.TELEGRAM_KEY, config.TELEGRAM_CHAT_ID, '`test_telegram_send`'
+        tests.mocks.TELEGRAM_KEY,
+        tests.mocks.TELEGRAM_CHAT_ID,
+        '`test_telegram_send`'
     )
     assert result
 
-@vcr.use_cassette(match_on=['host'])
+@vcr.use_cassette()
 def test_weather_get_now():
     temperatures = lib.basic_python3.weather_get_now()
     are_nums = all([
@@ -40,6 +43,12 @@ def test_weather_get_now():
     ])
     assert temperatures != []
     assert are_nums
+
+def test_weather_get_rand():
+    temperatures = lib.basic_python3.weather_get_rand()
+    are_floats = all([ isinstance(x, float) for x in temperatures ])
+    assert temperatures != []
+    assert are_floats
 
 def test_strftime():
     time_string = '2016-01-10T13:30:33'
