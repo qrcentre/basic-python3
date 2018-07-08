83bc79a90bc513eeb0d98a211f34db145d9a33d7
ning <ningyuan.sg@gmail.com>
2018-07-08 10:59:23 +0000

    Add telegram_get_updates and split into 3 parts
    commit 37c8d5faefb78a3f993fe853c21b2a2499d0f679
    Author: ning <ningyuan.sg@gmail.com>
    Date:   Sun Jul 8 10:56:50 2018 +0000
    
        Split sample scripts into smaller parts
    
    commit 8b54965b33bc5ae754d8dd47e6cb31792864d103
    Author: ning <ningyuan.sg@gmail.com>
    Date:   Sun Jul 8 08:24:38 2018 +0000
    
        Add telegram_get_updates
    
    commit 85af61157e4fec5d3eb80c3cf926de802a78cd30
    Author: ning <ningyuan.sg@gmail.com>
    Date:   Sat Jul 7 14:22:30 2018 +0000
    
        Prefix 'private' functions with underscores
    
    commit b6af3ee19fcdcc128b010a68503be14cd3fc4a07
    Author: ning <ningyuan.sg@gmail.com>
    Date:   Sat Jul 7 14:16:54 2018 +0000
    
        Adopt numpy style docstrings

--- tests/lib/test_basic_python3.py
+++ tests/lib/test_basic_python3.py
@@ -50,11 +50,11 @@ def test_weather_get_rand():
     assert temperatures != []
     assert are_floats
 
-def test_strftime():
+def _test_strftime():
     time_string = '2016-01-10T13:30:33'
     time_tuple = time.strptime(time_string, '%Y-%m-%dT%H:%M:%S')
     assert lib.basic_python3.strftime(time_tuple) == time_string
 
-def test_strftime_now():
+def _test_strftime_now():
     time_string = lib.basic_python3.strftime_now()
     assert re.match("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}", time_string)
